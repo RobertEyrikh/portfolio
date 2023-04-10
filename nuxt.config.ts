65e492d2205f674f3b5f0b8d87bb3344bdca2a03
Robert Eyrikh <RobertEyrikh@gmail.com>
2023-04-10 07:24:41 +0300

    change favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,9 @@ export default defineNuxtConfig({
   },
   app: {
     pageTransition: { name: "page", mode: "out-in" },
+    head: {
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
+    }
   },
   experimental: {
     inlineSSRStyles: false
